2d0ab42ac08e5b9bde1336fbd439242537e59cb6
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2021-02-08 23:07:27 -0300

    Conserto caminho da pasta localizaiton

--- front-end/app.js
+++ front-end/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     // Salvando path atual
     var loc = window.location.pathname;
+    console.log(loc)
 
     // Construindo path até a pasta com os textos traduzidos
     if(loc == "/" || loc == "/grain" || loc == "/grain/"){
