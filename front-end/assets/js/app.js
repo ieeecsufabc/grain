99d930c4a2cea76417ec5a072258cb2863f3a593
Enzo Shiraishi <enzo.shiraishi@gmail.com>
2021-08-24 12:29:01 -0300

    Implementar botao de fechar no popup de cookies

--- front-end/assets/js/app.js
+++ front-end/assets/js/app.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // Salvando path atual
     var loc = window.location.pathname;
     console.log(loc)
 
     // Encontrando path para salvar o cookie
-    if (loc.substring(1,6) == "grain"){
+    if (loc.substring(1, 6) == "grain") {
         console.log("github")
         cookiePath = "/grain/front-end/"
 
-        if(loc == "/grain/front-end/" || loc == "/grain/front-end/index.html"){
-            localizationPath = loc+"localization/web";
+        if (loc == "/grain/front-end/" || loc == "/grain/front-end/index.html") {
+            localizationPath = loc + "localization/web";
         } else {
             localizationPath = "../localization/web";
         }
@@ -18,8 +18,8 @@ $(document).ready(function(){
         console.log("local")
         cookiePath = "/front-end/"
 
-        if(loc == "/front-end/" || loc == "/front-end/index.html"){
-            localizationPath = loc+"localization/web";
+        if (loc == "/front-end/" || loc == "/front-end/index.html") {
+            localizationPath = loc + "localization/web";
         } else {
             localizationPath = "../localization/web";
         }
@@ -36,9 +36,9 @@ $(document).ready(function(){
     // Carrega cookie (string "language=...")
     var language = getCookie("language");
     // Salva o valor do cookie em uma variável language
-    console.log("Language:"+language)
+    console.log("Language:" + language)
 
-    if (language == "en"){
+    if (language == "en") {
         // Localização forçada
         $("[data-localize]").localize(localizationPath, { language: "en" })
     } else if (language == "pt") {
@@ -48,35 +48,40 @@ $(document).ready(function(){
         // Localização padrão é pt
         console.log("localization not found")
         // Salva cookie inicial
-        document.cookie = "language=pt;path="+cookiePath;
+        document.cookie = "language=pt;path=" + cookiePath;
         $("[data-localize]").localize(localizationPath, { language: "pt" })
     }
 
     // Mudança de localização com base no botão
-    if(!$('.check').is(":checked")) {
+    if (!$('.check').is(":checked")) {
         // Localização forçada
         $("[data-localize]").localize(localizationPath, { language: "pt" })
         // Salva cookie
-        document.cookie = "language=pt;path="+cookiePath;
+        document.cookie = "language=pt;path=" + cookiePath;
         $('.pt').addClass('checked')
     }
-    
-    $(".check").click(function(){
-        if($('.check').is(":checked")) {
+
+    $(".check").click(function () {
+        if ($('.check').is(":checked")) {
             $(".check").addClass("checkEN")
             // Localização forçada
             $("[data-localize]").localize(localizationPath, { language: "en" })
             // Salva cookie
-            document.cookie = "language=en;path="+cookiePath;
+            document.cookie = "language=en;path=" + cookiePath;
             $('.pt').removeClass('checked')
             $('.en').addClass('checked')
         } else {
             // Localização forçada
             $("[data-localize]").localize(localizationPath, { language: "pt" })
             // Salva cookie
-            document.cookie = "language=pt;path="+cookiePath;
+            document.cookie = "language=pt;path=" + cookiePath;
             $('.en').removeClass('checked')
             $('.pt').addClass('checked')
         }
     })
 });
+
+// desabilitar popup
+function disableCookiePopup() {
+    document.querySelector(".cookies-container").style.display = 'none';
+}
