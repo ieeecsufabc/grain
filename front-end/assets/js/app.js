d465777c614268ab5f1535b659412b274185e2e6
Enzo Shiraishi <enzo.shiraishi@gmail.com>
2021-08-24 16:13:17 -0300

    Refatorar o popup sobre cookies.

--- front-end/assets/js/app.js
+++ front-end/assets/js/app.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+$(document).ready(() => {
 
     // Salvando path atual
-    var loc = window.location.pathname;
-    console.log(loc)
+    let loc = window.location.pathname;
+    console.log(loc);
 
     // Encontrando path para salvar o cookie
     if (loc.substring(1, 6) == "grain") {
@@ -30,11 +30,11 @@ $(document).ready(function () {
         const value = `; ${document.cookie}`;
         const parts = value.split(`; ${name}=`);
         //if (parts.length === 2) 
-        return parts.pop().split(';').shift();
+        return parts.pop().split(";").shift();
     }
 
     // Carrega cookie (string "language=...")
-    var language = getCookie("language");
+    let language = getCookie("language");
     // Salva o valor do cookie em uma variável language
     console.log("Language:" + language)
 
@@ -53,35 +53,43 @@ $(document).ready(function () {
     }
 
     // Mudança de localização com base no botão
-    if (!$('.check').is(":checked")) {
+    if (!$(".check").is(":checked")) {
         // Localização forçada
         $("[data-localize]").localize(localizationPath, { language: "pt" })
         // Salva cookie
         document.cookie = "language=pt;path=" + cookiePath;
-        $('.pt').addClass('checked')
+        $(".pt").addClass("checked")
     }
 
-    $(".check").click(function () {
-        if ($('.check').is(":checked")) {
+    $(".check").click(() => {
+        if ($(".check").is(":checked")) {
             $(".check").addClass("checkEN")
             // Localização forçada
             $("[data-localize]").localize(localizationPath, { language: "en" })
             // Salva cookie
             document.cookie = "language=en;path=" + cookiePath;
-            $('.pt').removeClass('checked')
-            $('.en').addClass('checked')
+            $(".pt").removeClass("checked")
+            $(".en").addClass("checked")
         } else {
             // Localização forçada
             $("[data-localize]").localize(localizationPath, { language: "pt" })
             // Salva cookie
             document.cookie = "language=pt;path=" + cookiePath;
-            $('.en').removeClass('checked')
-            $('.pt').addClass('checked')
+            $(".en").removeClass("checked")
+            $(".pt").addClass("checked")
         }
     })
-});
 
-// desabilitar popup
-function disableCookiePopup() {
-    document.querySelector(".cookies-container").style.display = 'none';
-}
+    document.querySelector(".cookies-container").style.display = (getCookie("acceptCookies") === "") ? "block" : "none";
+
+    let acceptCookiesButton = document.querySelector(".button-accept");
+    acceptCookiesButton.addEventListener("click", () => {
+        console.log("Resultado: " + getCookie("acceptCookies"));
+        let acceptCookies = getCookie("acceptCookies");
+        if (acceptCookies === "") {
+            document.cookie = "acceptCookies=true;path=" + cookiePath;
+        }
+        document.querySelector(".cookies-container").style.display = "none";
+    });
+
+});
